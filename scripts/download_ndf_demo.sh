7a5c2a9338bd85cb69331d178c3b2c3ea6018dfd
6cyc6 <85584065+6cyc6@users.noreply.github.com>
2024-09-11 14:46:07 +0200

    Update download_ndf_demo.sh

--- scripts/download_ndf_demo.sh
+++ scripts/download_ndf_demo.sh
@@ -1,7 +1,7 @@
-if [ -z $MFIM_DIR ]; then echo 'Please source "mimo_env.sh" first'
+if [ -z $MIMO_DIR ]; then echo 'Please source "mimo_env.sh" first'
 else
   gdown 'https://docs.google.com/uc?export=download&id=1rj4FNV5-cr7gHd0jjfvxyaEarD901aF1' -O data.tgz
-  tar -xvzf data.tgz -C $MFIM_DIR/eval/ndf
+  tar -xvzf data.tgz -C $MIMO_DIR/eval/ndf
   rm data.tgz
   echo 'Finished!'
 fi
