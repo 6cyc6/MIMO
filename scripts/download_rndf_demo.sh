267f0c69acc5b600946de709ae026b387317dfda
6cyc6 <85584065+6cyc6@users.noreply.github.com>
2024-09-11 14:46:54 +0200

    Update download_rndf_demo.sh

--- scripts/download_rndf_demo.sh
+++ scripts/download_rndf_demo.sh
@@ -1,7 +1,7 @@
-if [ -z $MFIM_DIR ]; then echo 'Please source "mimo_env.sh" first'
+if [ -z $MIMO_DIR ]; then echo 'Please source "mimo_env.sh" first'
 else
   gdown 'https://docs.google.com/uc?export=download&id=13rgepmqdN3pvn2OaAyvJND84zouP2Xue' -O data.tgz
-  tar -xvzf data.tgz -C $MFIM_DIR/eval/rndf
+  tar -xvzf data.tgz -C $MIMO_DIR/eval/rndf
   rm data.tgz
   echo 'Finished!'
 fi
