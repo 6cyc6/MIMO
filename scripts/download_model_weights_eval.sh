959b6e8da866f129c28bfa43c22d6d0602094330
6cyc6 <85584065+6cyc6@users.noreply.github.com>
2024-09-11 14:45:55 +0200

    Update download_model_weights_eval.sh

--- scripts/download_model_weights_eval.sh
+++ scripts/download_model_weights_eval.sh
@@ -1,8 +1,8 @@
-if [ -z $MFIM_DIR ]; then echo 'Please source "mimo_env.sh" first'
+if [ -z $MIMO_DIR ]; then echo 'Please source "mimo_env.sh" first'
 else
   wget --no-check-certificate 'https://drive.google.com/uc?export=download&id=1buAWXe-E8qTZTuUTpA4EiM76y8C32py0' -O model_weights.tar
-  tar -xvf model_weights.tar -C $MFIM_DIR/eval/ndf
-  tar -xvf model_weights.tar -C $MFIM_DIR/eval/rndf
+  tar -xvf model_weights.tar -C $MIMO_DIR/eval/ndf
+  tar -xvf model_weights.tar -C $MIMO_DIR/eval/rndf
   rm model_weights.tar
   echo 'Finished!'
 fi
